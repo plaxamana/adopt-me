e4678853cd300ed20f9fc838552dca979acdfef5
Philip Laxamana <philip.laxamana@hotmail.com>
2021-06-23 20:57:07 -0400

    Making the Carousel interactive with handleIndexClick

--- src/Carousel.js
+++ src/Carousel.js
@@ -10,6 +10,12 @@ class Carousel extends Component {
     images: ['http://pets-images.dev-apis.com/pets/none.jpg']
   }
 
+  handleIndexClick = (e) => {
+    this.setState({
+      active: +e.target.dataset.index // + coerces it to a number (from a string)
+    })
+  }
+
   render() {
     // props is read-only from parent
     // state is mutable from within its component
@@ -20,9 +26,12 @@ class Carousel extends Component {
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img 
               key={photo}
               src={photo}
+              data-index={index}
+              onClick={this.handleIndexClick}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
