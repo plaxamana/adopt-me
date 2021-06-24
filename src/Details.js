6d34824bcf12e7a419648e1dc720cb389d5d7d81
Philip Laxamana <philip.laxamana@hotmail.com>
2021-06-23 21:37:07 -0400

    useContext hook for theme

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { withRouter } from "react-router";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends Component {
   state = { loading: true };
@@ -27,14 +28,19 @@ class Details extends Component {
       return <h2>Loading..</h2>;
     }
 
-    const { animal, breed, city, state, description, name, images } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
     return (
       <div className="details">
         <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {([theme]) => (
+              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
